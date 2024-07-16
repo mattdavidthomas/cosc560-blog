ffda711fdeb94be605b63805754fd79192be718a
Matthew Thomas <matthew.thomas@stpauls.nsw.edu.au>
2024-07-16 20:58:46 +1000

    added new posts and photos controllers and associated view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts/show', [PostController::class, 'show'])->name('posts.show');
+*/
+
+Route::resource('posts', PostController::class);
+Route::resource('photos', PhotoController::class);
